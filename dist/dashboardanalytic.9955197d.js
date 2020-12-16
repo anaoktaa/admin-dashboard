27c8dc6ef11d30bfb47e00014008ce025af1b03b
anaoktaa <ana.oktaviana.ana@gmail.com>
2020-12-16 17:47:52 +0700

    build

--- dist/dashboardanalytic.9955197d.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],[],[[193,1,2,5,9,0,3,6]]]);
--- dist/dashboardcommerce.a7094ed1.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[13],[],[[385,1,2,5,10,0,4,7]]]);
