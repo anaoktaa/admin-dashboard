c286aa510441af23390e240ba328487d88d76c9f
anaoktaa <ana.oktaviana.ana@gmail.com>
2020-12-16 17:58:02 +0700

    addting text on loader

--- src/components/loader/loader.component.js
+++ src/components/loader/loader.component.js
@@ -7,7 +7,9 @@ import { LoaderContainer } from './loader.styles';
 const Loader = () => {
     return (
         <LoaderContainer>
-            <img src={chunk}/>
+            <img src={chunk}/><br/>
+            <p className='color5d no-margin-no-padding'>Please wait while we load all the components examples</p>
+            <p className='color98'>Because this is a demonstration, we load at once all the Dashboards examples. This wouldn't happen in a real live app!</p>
         </LoaderContainer>
     )
 };
